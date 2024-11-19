c5851b39966e6ab6b00d63e11f6392cf84c8208b
valenlopezzz04 <140413463+valenlopezzz04@users.noreply.github.com>
2024-11-18 22:32:19 -0500

    eliminar carpeta del backend

--- inventario-backend/testconnection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const uri = "mongodb+srv://valejalopez444:valentina@gestioninventario.o72zu.mongodb.net/inventario?retryWrites=true&w=majority&appName=GestionInventario";
-
-mongoose.connect(uri)
-  .then(() => console.log("Conectado a MongoDB Atlas"))
-  .catch(error => console.error("Error conectando a MongoDB:", error));
